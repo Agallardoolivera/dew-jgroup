228abd5f5170bff3b071ede88cdd6a3f6b9770f2
alfredo.go <alfredo.go@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-06-12 22:55:57 +0000

    Packages Change

---
+++ trunk/JGroup_Web/src/java/LineaArticulo.java
@@ -0,0 +1,28 @@
+
+public class LineaArticulo {
+
+    private String CoArticulo;
+
+    private String CoLinea;
+
+    public LineaArticulo () {
+    }
+
+    public String getCoArticulo () {
+        return CoArticulo;
+    }
+
+    public void setCoArticulo (String val) {
+        this.CoArticulo = val;
+    }
+
+    public String getCoLinea () {
+        return CoLinea;
+    }
+
+    public void setCoLinea (String val) {
+        this.CoLinea = val;
+    }
+
+}
+
---
+++ trunk/JGroup_Web/src/java/ProveedorLinea.java
@@ -0,0 +1,28 @@
+
+public class ProveedorLinea {
+
+    private String CoProveedor;
+
+    private String CoLinea;
+
+    public ProveedorLinea () {
+    }
+
+    public String getCoLinea () {
+        return CoLinea;
+    }
+
+    public void setCoLinea (String val) {
+        this.CoLinea = val;
+    }
+
+    public String getCoProveedor () {
+        return CoProveedor;
+    }
+
+    public void setCoProveedor (String val) {
+        this.CoProveedor = val;
+    }
+
+}
+
