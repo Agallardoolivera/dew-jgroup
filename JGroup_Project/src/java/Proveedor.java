1616a195ae78a660bdb450cf61e6f5e3f131ce60
alfredo.go <alfredo.go@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-06-12 21:29:41 +0000

    First Version

---
+++ JGroup_Project/src/java/Proveedor.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList; 
+
+public class Proveedor {
+
+    private String Codigo;
+
+    private String Ruc;
+
+    private String RazonSocial;
+
+    private String Correo;
+
+    private String ClaveAcceso;
+
+    private ArrayList<ProveedorLinea> proveedorLinea;
+
+    public Proveedor () {
+    }
+
+    public String getClaveAcceso () {
+        return ClaveAcceso;
+    }
+
+    public void setClaveAcceso (String val) {
+        this.ClaveAcceso = val;
+    }
+
+    public String getCodigo () {
+        return Codigo;
+    }
+
+    public void setCodigo (String val) {
+        this.Codigo = val;
+    }
+
+    public String getCorreo () {
+        return Correo;
+    }
+
+    public void setCorreo (String val) {
+        this.Correo = val;
+    }
+
+    public String getRazonSocial () {
+        return RazonSocial;
+    }
+
+    public void setRazonSocial (String val) {
+        this.RazonSocial = val;
+    }
+
+    public String getRuc () {
+        return Ruc;
+    }
+
+    public void setRuc (String val) {
+        this.Ruc = val;
+    }
+
+    public ArrayList<ProveedorLinea> getProveedorLinea () {
+        return proveedorLinea;
+    }
+
+    public void setProveedorLinea (ArrayList<ProveedorLinea> val) {
+        this.proveedorLinea = val;
+    }
+
+}
+
