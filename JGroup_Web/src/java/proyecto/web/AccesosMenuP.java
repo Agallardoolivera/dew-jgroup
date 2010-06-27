b0bf8c99c78f8b459cc896fc31c6caacec506917
angela.caceres <angela.caceres@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-06-27 00:09:16 +0000

    Llamado de ventana con el mismo servlet

---
+++ JGroup_Web/src/java/proyecto/web/AccesosMenuP.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.web;
+
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import org.springframework.web.servlet.view.RedirectView;
+/** 
+ *
+ * @author pcsidrod
+ */
+
+public class AccesosMenuP extends MultiActionController {
+
+    public AccesosMenuP() {
+        //Initialize controller properties here or 
+        //in the Web Application Context
+        //setCommandClass(MyCommand.class);
+        //setCommandName("MyCommandName");
+        //setSuccessView("successView");
+        //setFormView("formView");
+
+    }
+
+
+
+
+    public ModelAndView Articulo(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("Articulo");
+    }
+
+
+
+    protected void doSubmitAction(Object command) throws Exception {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //Use onSubmit instead of doSubmitAction 
+    //when you need access to the Request, Response, or BindException objects
+    /*
+    @Override
+    protected ModelAndView onSubmit(
+    HttpServletRequest request,
+    HttpServletResponse response,
+    Object command,
+    BindException errors) throws Exception {
+    ModelAndView mv = new ModelAndView(getSuccessView());
+    //Do something...
+    return mv;
+    }
+     */
+
+
+
+}
