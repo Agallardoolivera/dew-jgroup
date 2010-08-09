37759c8d461d457d803d06be0893af2a5ca10b4a
oscarchavarric@gmail.com <oscarchavarric@gmail.com@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-08-09 22:13:38 +0000

    avance oscar

--- JGroup_Web/src/java/proyecto/service/UsuarioService.java
+++ JGroup_Web/src/java/proyecto/service/UsuarioService.java
@@ -1,10 +1,16 @@
 package proyecto.service;
 
+import java.util.Collection;
 import proyecto.excepcion.DAOExcepcion;
 import proyecto.excepcion.LoginExcepcion;
 import proyecto.modelo.Usuario;
 
 public interface UsuarioService {
 
+    public Collection<Usuario> buscarPorNombre(String nombres) throws DAOExcepcion;
+    public Usuario insertar(Usuario vo) throws DAOExcepcion;
+    public Usuario obtener(int codigo) throws DAOExcepcion;
+    public Usuario actualizar(Usuario vo) throws DAOExcepcion;
+    public void eliminar(int codigo) throws DAOExcepcion;
     public Usuario validar(int Co_Usuario, String clave) throws DAOExcepcion, LoginExcepcion;
 }
