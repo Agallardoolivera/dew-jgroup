31070fe97f2bbb1af207b8c7288a8c30f28435fb
oscarchavarric@gmail.com <oscarchavarric@gmail.com@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-08-09 22:13:03 +0000

    avance oscar

--- JGroup_Web/src/java/proyecto/modelo/Usuario.java
+++ JGroup_Web/src/java/proyecto/modelo/Usuario.java
@@ -12,22 +12,17 @@ public class Usuario {
 
     private int co_Usuario;
     private String clave;
-    private String nombres;
-    private String area;
-    private String txt_nom;
-    private String txt_ape;
-    private String txt_dni;
+    private int estado;
 
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+   
     public Usuario(){}
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
 
     public String getClave() {
         return clave;
@@ -45,36 +40,6 @@ public class Usuario {
         this.co_Usuario = co_Usuario;
     }
 
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-        public String getTxt_ape() {
-        return txt_ape;
-    }
-
-    public void setTxt_ape(String txt_ape) {
-        this.txt_ape = txt_ape;
-    }
-
-    public String getTxt_dni() {
-        return txt_dni;
-    }
-
-    public void setTxt_dni(String txt_dni) {
-        this.txt_dni = txt_dni;
-    }
-
-    public String getTxt_nom() {
-        return txt_nom;
-    }
-
-    public void setTxt_nom(String txt_nom) {
-        this.txt_nom = txt_nom;
-    }
+   
     
 }
