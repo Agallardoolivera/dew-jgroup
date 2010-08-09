33256cd4a5668b8b4aa1ae07cdafdd58a8b55079
oscarchavarric@gmail.com <oscarchavarric@gmail.com@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-08-09 22:12:43 +0000

    avance oscar

--- trunk/JGroup_Web/src/java/proyecto/dao/UsuarioDAO.java
+++ trunk/JGroup_Web/src/java/proyecto/dao/UsuarioDAO.java
@@ -1,11 +1,17 @@
 package proyecto.dao;
 
+import java.util.Collection;
 import proyecto.excepcion.DAOExcepcion;
 import proyecto.excepcion.LoginExcepcion;
 import proyecto.modelo.Usuario;
 
 public interface UsuarioDAO {
 
+    public Collection<Usuario> buscarPorNombre(String nombres) throws DAOExcepcion;
+    public Usuario insertar(Usuario vo) throws DAOExcepcion;
+    public Usuario obtener(int codigo) throws DAOExcepcion;
+    public Usuario actualizar(Usuario vo) throws DAOExcepcion;
+    public void eliminar(int codigo) throws DAOExcepcion;
     public Usuario validar(int Co_Usuario, String clave) throws DAOExcepcion, LoginExcepcion;
     
 }
--- trunk/JGroup_Web/src/java/proyecto/dao/UsuarioDAOImpl.java
+++ trunk/JGroup_Web/src/java/proyecto/dao/UsuarioDAOImpl.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.ArrayList;
 import javax.sql.DataSource;
 import proyecto.excepcion.DAOExcepcion;
 import proyecto.excepcion.LoginExcepcion;
@@ -23,7 +25,7 @@ public class UsuarioDAOImpl extends BaseDAO implements UsuarioDAO {
 
     public Usuario validar(int Co_Usuario, String clave) throws DAOExcepcion, LoginExcepcion {
         System.out.println("UsuarioDAOImpl: validar: Co_Usuario: " + Co_Usuario + ", clave: " + clave);
-        String query = "select Co_Usuario, nombres, paterno, materno, correo, direccion, telefono, estado from usuario where Co_Usuario=? and clave=?";
+        String query = "select Co_Usuario, clave from usuario where Co_Usuario=? and clave=?";
         Connection con = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -37,9 +39,8 @@ public class UsuarioDAOImpl extends BaseDAO implements UsuarioDAO {
 
             if (rs.next()) {
                
-                vo.setNombres(rs.getString("nombres"));
-                vo.setTxt_ape(rs.getString("paterno"));
-                vo.setNombres(rs.getString("materno"));
+                vo.setClave(rs.getString("clave"));
+                
                
             } else {
                 throw new LoginExcepcion("No existe");
@@ -57,5 +58,135 @@ public class UsuarioDAOImpl extends BaseDAO implements UsuarioDAO {
         }
         return vo;
     }
+
+     public Collection<Usuario> buscarPorNombre(String nombres)
+            throws DAOExcepcion {
+        String query = "select Co_Usuario, clave from usuario where estado=0 and Co_Usuario like ?";
+        Collection<Usuario> lista = new ArrayList<Usuario>();
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            con = dataSource.getConnection();
+            stmt = con.prepareStatement(query);
+            stmt.setString(1, "%" + nombres + "%");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Usuario vo = new Usuario();
+                vo.setCo_Usuario(rs.getInt("Co_Usuario"));
+                vo.setClave(rs.getString("clave"));
+               
+                lista.add(vo);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new DAOExcepcion(e.getMessage());
+        } finally {
+            this.cerrarResultSet(rs);
+            this.cerrarStatement(stmt);
+            this.cerrarConexion(con);
+        }
+        System.out.println(lista.size());
+        return lista;
+    }
+
+     public Usuario insertar(Usuario vo) throws DAOExcepcion {
+        String query = "INSERT INTO usuario(Co_Usuario,clave,estado) VALUES (?,?,?)";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            con = dataSource.getConnection();
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, vo.getCo_Usuario());
+            stmt.setString(2, vo.getClave());
+            stmt.setInt(3, vo.getEstado());
+            int i = stmt.executeUpdate();
+            if (i != 1) {
+                throw new SQLException("No se pudo insertar");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new DAOExcepcion(e.getMessage());
+        } finally {
+            this.cerrarResultSet(rs);
+            this.cerrarStatement(stmt);
+            this.cerrarConexion(con);
+        }
+        return vo;
+    }
+
+     public Usuario obtener(int codigo) throws DAOExcepcion {
+        Usuario vo = new Usuario();
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            String query = "select Co_Usuario, clave from usuario where Co_Usuario=?";
+            con = dataSource.getConnection();
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, codigo);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                vo.setCo_Usuario(rs.getInt("Co_Usuario"));
+                vo.setClave(rs.getString("clave"));
+                
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new DAOExcepcion(e.getMessage());
+        } finally {
+            this.cerrarResultSet(rs);
+            this.cerrarStatement(stmt);
+            this.cerrarConexion(con);
+        }
+        return vo;
+    }
+
+     public Usuario actualizar(Usuario vo) throws DAOExcepcion {
+        String query = "update usuario set clave=? where Co_Usuario=?";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        try {
+            con = dataSource.getConnection();
+            stmt = con.prepareStatement(query);
+            stmt.setString(1, vo.getClave());
+            stmt.setInt(2, vo.getCo_Usuario());
+            int i = stmt.executeUpdate();
+            if (i != 1) {
+                throw new SQLException("No se pudo actualizar");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new DAOExcepcion(e.getMessage());
+        } finally {
+            this.cerrarStatement(stmt);
+            this.cerrarConexion(con);
+        }
+        return vo;
+    }
+
+
+     public void eliminar(int codigo) throws DAOExcepcion {
+        String query = "update usuario set estado=1 where Co_Usuario=?";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        try {
+            con = dataSource.getConnection();
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, codigo);
+            int i = stmt.executeUpdate();
+            if (i != 1) {
+                throw new SQLException("No se pudo eliminar");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new DAOExcepcion(e.getMessage());
+        } finally {
+            this.cerrarStatement(stmt);
+            this.cerrarConexion(con);
+        }
+    }
+
 }
 
