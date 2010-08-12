3de5746fe38b8b371647395584e6a548fc1721a4
angela.caceres <angela.caceres@96dde5d3-fa31-8190-30c0-f974ac2444af>
2010-08-12 08:52:55 +0000

    Actualizando el registro de cotizaciones

--- JGroup_Web/src/java/proyecto/service/InvitacionDetalleServiceImpl.java
+++ JGroup_Web/src/java/proyecto/service/InvitacionDetalleServiceImpl.java
@@ -22,30 +22,27 @@ public class InvitacionDetalleServiceImpl implements InvitacionDetalleService {
     }
 
     public void setInvitacionDAO(InvitacionDetalleDAO InvitacionDetalleDAO) {
-        this.InvitacionDAO = InvitacionDAO;
+        this.InvitacionDetalleDAO = InvitacionDetalleDAO;
     }
 
-    public Invitacion insertar(Invitacion vo) throws DAOExcepcion {
-        return InvitacionDAO.insertar(vo);
+    public DetalleInvitacion insertar(DetalleInvitacion vo) throws DAOExcepcion {
+        return InvitacionDetalleDAO.insertar(vo);
     }
 
-    public Collection<Invitacion> buscarPorNombre(String No_Invitacion) throws DAOExcepcion {
-        return InvitacionDAO.buscarPorNombre(No_Invitacion);
+    public Collection<DetalleInvitacion> buscarPorNombre(int No_Invitacion) throws DAOExcepcion {
+        return InvitacionDetalleDAO.buscarPorNombre(No_Invitacion);
     }
 
-    public Collection<Invitacion> listar() throws DAOExcepcion {
-        return InvitacionDAO.listar();
+    public Collection<DetalleInvitacion> listar() throws DAOExcepcion {
+        return InvitacionDetalleDAO.listar();
     }
 
-    public Invitacion obtener(int Co_Invitacion) throws DAOExcepcion {
-        return InvitacionDAO.obtener(Co_Invitacion);
+    public DetalleInvitacion obtener(int Co_Invitacion) throws DAOExcepcion {
+        return InvitacionDetalleDAO.obtener(Co_Invitacion);
     }
 
     public void eliminar(int Co_Invitacion) throws DAOExcepcion {
-        InvitacionDAO.eliminar(Co_Invitacion);
+        InvitacionDetalleDAO.eliminar(Co_Invitacion);
     }
 
-    public Invitacion actualizar(Invitacion vo) throws DAOExcepcion {
-        return InvitacionDAO.actualizar(vo);
-    }
 }
